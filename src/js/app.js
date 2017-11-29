9ae462a4ccde72027a1d5ede2f554290e896e2f3
AurecchiaP <paolo@aurecchia.ch>
2017-11-29 20:39:41 +0100

    updated libraries plus minor things

--- src/js/app.js
+++ src/js/app.js
@@ -8,7 +8,7 @@ App = {
       var petsRow = $('#petsRow');
       var petTemplate = $('#petTemplate');
 
-      for (i = 0; i < data.length; i ++) {
+      for (i = 0; i < data.length; i++) {
         petTemplate.find('.panel-title').text(data[i].name);
         petTemplate.find('img').attr('src', data[i].picture);
         petTemplate.find('.pet-breed').text(data[i].breed);
@@ -24,30 +24,30 @@ App = {
   },
 
   initWeb3: function() {
-      // Is there is an injected web3 instance?
-      if (typeof web3 !== 'undefined') {
-        App.web3Provider = web3.currentProvider;
-      } else {
-        // If no injected web3 instance is detected, fallback to the TestRPC
-        App.web3Provider = new Web3.providers.HttpProvider('http://localhost:8545');
-      }
-      web3 = new Web3(App.web3Provider);
+    // Is there is an injected web3 instance?
+    if (typeof web3 !== 'undefined') {
+      App.web3Provider = web3.currentProvider;
+    } else {
+      // If no injected web3 instance is detected, fallback to the TestRPC
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:8545');
+    }
+    web3 = new Web3(App.web3Provider);
 
     return App.initContract();
   },
 
   initContract: function() {
-      $.getJSON('Adoption.json', function(data) {
-        // Get the necessary contract artifact file and instantiate it with truffle-contract
-        var AdoptionArtifact = data;
-        App.contracts.Adoption = TruffleContract(AdoptionArtifact);
+    $.getJSON('Adoption.json', function(data) {
+      // Get the necessary contract artifact file and instantiate it with truffle-contract
+      var AdoptionArtifact = data;
+      App.contracts.Adoption = TruffleContract(AdoptionArtifact);
 
-        // Set the provider for our contract
-        App.contracts.Adoption.setProvider(App.web3Provider);
+      // Set the provider for our contract
+      App.contracts.Adoption.setProvider(App.web3Provider);
 
-        // Use our contract to retrieve and mark the adopted pets
-        return App.markAdopted();
-      });
+      // Use our contract to retrieve and mark the adopted pets
+      return App.markAdopted();
+    });
 
     return App.bindEvents();
   },
@@ -57,22 +57,22 @@ App = {
   },
 
   markAdopted: function(adopters, account) {
-      var adoptionInstance;
+    var adoptionInstance;
 
-      App.contracts.Adoption.deployed().then(function(instance) {
-        adoptionInstance = instance;
+    App.contracts.Adoption.deployed().then(function(instance) {
+      adoptionInstance = instance;
 
-        return adoptionInstance.getAdopters.call();
-      }).then(function(adopters) {
-          console.log(adopters);
-        for (i = 0; i < adopters.length; i++) {
-          // if (adopters[i] !== '0x') {
-          //   $('.panel-pet').eq(i).find('button').text('Success').attr('disabled', true);
-          // }
-        }
-      }).catch(function(err) {
-        console.log(err.message);
-      });
+      return adoptionInstance.getAdopters.call();
+    }).then(function(adopters) {
+      console.log(adopters);
+      for (i = 0; i < adopters.length; i++) {
+        // if (adopters[i] !== '0x') {
+        //   $('.panel-pet').eq(i).find('button').text('Success').attr('disabled', true);
+        // }
+      }
+    }).catch(function(err) {
+      console.log(err.message);
+    });
   },
 
   handleAdopt: function(event) {
@@ -93,7 +93,9 @@ App = {
         adoptionInstance = instance;
 
         // Execute adopt as a transaction by sending account
-        return adoptionInstance.adopt(petId, {from: account});
+        return adoptionInstance.adopt(petId, {
+          from: account
+        });
       }).then(function(result) {
         return App.markAdopted();
       }).catch(function(err) {
--- src/js/util.js
+++ src/js/util.js
@@ -1,74 +1,113 @@
-
 // stone class
 class Stone {
-    constructor(x, y, color) {
-        this.x = x;
-        this.y = y;
-        this.color = color;
-    }
+  constructor(x, y, color) {
+    this.x = x;
+    this.y = y;
+    this.color = color;
+  }
 }
 
-// create an empty array for the board 19x19
-let board = [];
+class Board {
+  constructor(data) {
+    this.data = data;
+  }
 
-for (let i = 0; i < 19*19; i++) {
-    board[i] = 0;
+  updateSize() {
+    this.width = boardDiv[0].clientWidth;
+    this.cellWidth = this.width / 19;
+  }
 }
 
 var boardDiv = $('#board');
 
-var placedStone = new Stone(0,0,'white');
+var placedStone = new Stone(0, 0, 'white');
+
+// create an empty array for the board 19x19
+// let board = [];
+var board = new Board([], 0);
+
+for (let i = 0; i < 19; i++) {
+  for (let j = 0; j < 19; j++) {
+    // board[i] = 0;
+    if ((i * 19 + j) % 2 == 0) {
+      board.data[i * 19 + j] = 0;
+    } else {
+      board.data[i * 19 + j] = 1;
+    }
+  }
+}
+
+// for (let i = 0; i < 19; i++) {
+//   for (let j = 0; j < 19; j++) {
+//     board.updateSize();
+//     let stone;
+//     if (board.data[i * 19 + j] == 0) {
+//       stone = $('<div class="stone white as" id="stone' + (i * 19 + j) + '"></div>');
+//     } else {
+//       stone = $('<div class="stone black as" id="stone' + (i * 19 + j) + '"></div>');
+//     }
+//     stone.css({
+//       display: 'block',
+//       top: i * board.cellWidth + 'px',
+//       left: j * board.cellWidth + 'px'
+//     });
+//     boardDiv.append(stone);
+//   }
+// }
 
 // when the cursor hovers the board, show where the stone would be placed
 boardDiv.on('mousemove', this, function() {
-    // relative position of the board
-    let offset = $(this).offset();
-    // find the cursor position relative to the board
-    let x = event.pageX - offset.left;
-    let y = event.pageY - offset.top;
-    // width (and height) of the board
-    let w = this.clientWidth;
-    let cellWidth = w/19;
-    let cellX = Math.floor(19*x/w);
-    let cellY = Math.floor(19*y/w);
 
-    if(cellX < 19 && cellY < 19) {
-        $('#hoverStone').css({
-            display: 'block',
-            top: cellY * cellWidth + 'px',
-            left: cellX * cellWidth + 'px'
-        });
-    }
+  let offset = $(this).offset();
+
+  let x = event.pageX - offset.left;
+  let y = event.pageY - offset.top;
+
+  let cellX = Math.floor(19 * x / board.width);
+  let cellY = Math.floor(19 * y / board.width);
+
+  if (cellX < 19 && cellY < 19) {
+    $('#hoverStone').css({
+      display: 'block',
+      top: cellY * board.cellWidth + 'px',
+      left: cellX * board.cellWidth + 'px'
+    });
+  }
 });
 
 // when the cursor leaves the board, hide the stone
 boardDiv.on('mouseleave', this, function() {
-    $('#hoverStone').css({
-        display: 'none'
-    });
+  $('#hoverStone').css({
+    display: 'none'
+  });
 });
 
 // when we click the board, put a stone on the selected intersection
 boardDiv.on('click', this, function() {
-    // relative position of the board
-    let offset = $(this).offset();
-    // find the cursor position relative to the board
-    let x = event.pageX - offset.left;
-    let y = event.pageY - offset.top;
-    // width (and height) of the board
-    let w = this.clientWidth;
-    let cellWidth = w/19;
-    let cellX = Math.floor(19*x/w);
-    let cellY = Math.floor(19*y/w);
+  // relative position of the board
+  board.updateSize();
+  let offset = $(this).offset();
+  // find the cursor position relative to the board
+  let x = event.pageX - offset.left;
+  let y = event.pageY - offset.top;
+  // width (and height) of the board
 
-    if(cellX < 19 && cellY < 19) {
-        $('#placedStone').css({
-            display: 'block',
-            top: cellY * cellWidth + 'px',
-            left: cellX * cellWidth + 'px'
-        });
-        placedStone.x = cellX;
-        placedStone.y = cellY;
-        console.log("placed stone: ", placedStone.x, placedStone.y);
-    }
+  let cellX = Math.floor(19 * x / board.width);
+  let cellY = Math.floor(19 * y / board.width);
+
+  if (cellX < 19 && cellY < 19) {
+    $('#placedStone').css({
+      display: 'block',
+      top: cellY * board.cellWidth + 'px',
+      left: cellX * board.cellWidth + 'px'
+    });
+    placedStone.x = cellX;
+    placedStone.y = cellY;
+    console.log("placed stone: ", placedStone.x, placedStone.y);
+  }
+});
+
+$( window ).resize(function() {
+  // boardDiv.empty();
+  // board.updateSize();
 });
