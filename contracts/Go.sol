c819b7e20816254462ce27b2b4df3d9fb354bb57
AurecchiaP <paolo@aurecchia.ch>
2017-12-06 09:40:30 +0100

    can take turns

--- contracts/Go.sol
+++ contracts/Go.sol
@@ -30,7 +30,7 @@ contract Go {
 
   function newGame() public {
     /* TODO create a new contract game and push its address */
-    games.push(0x0);
+    games.push(owner);
   }
 
   function getGames() public view returns (address[]) {
@@ -38,29 +38,33 @@ contract Go {
   }
 
 
-  function move(uint pos) public ownerRestricted() {
-    games.push(owner);
-
-    game.turn = game.turn + 1;
-    game.board[pos] = game.turn;
-    game.turn = game.turn % 2;
+  function move(uint pos) public {
+    if(game.turn == 0 && owner == msg.sender) {
+      game.turn = game.turn + 1;
+      game.board[pos] = game.turn;
+      game.turn = game.turn % 2;
+    } else if(game.turn == 1 && owner != msg.sender) {
+      game.turn = game.turn + 1;
+      game.board[pos] = game.turn;
+      game.turn = game.turn % 2;
+    }
   }
 
   function getBoard() public view returns (uint[361]) {
     return game.board;
   }
 
-  function setVariable (uint pos) public {
-    if(pos < 361){
+  function setVariable(uint pos) public {
+    if(pos < 361) {
       game.board[pos] = 1;
 
     }
   }
-  function getMove(uint pos) public pure returns(uint){
-    if(pos < 361){
+  function getMove(uint pos) public pure returns(int) {
+    if(pos < 361) {
       return pos;
     }
-    return 404; // illegal position
+    return -1; // illegal position
   }
 }
 
